adeeaa4a9aa978516a7575caaff509d238561d6b
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2023-01-22 19:13:08 +0900

    context errgroup [add]

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	// serverの情報
+	s := &http.Server{
+		Addr: "18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+	}
+
+	//
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// 複数の非同期処理を最後にまとめてエラーハンドリングする
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルから通知を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Goメソッドで起動した別ゴルーチンを待つ
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
