ac1efecdf639b51cf83d1409660dbab031da6ada
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2023-01-25 01:39:33 +0900

    [add] entity & store

---
+++ store/store.go
@@ -0,0 +1,24 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/shifujito/todo_app/entity"
+)
+
+var (
+	Tasks       = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
