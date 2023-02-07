adc8dc84fbff9551795779f60b1109cae7f84b6f
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2023-02-07 16:39:56 +0900

    fix

--- front/src/components/ingredientsList.tsx
+++ front/src/components/ingredientsList.tsx
@@ -3,6 +3,30 @@ type ingredientsProps = {
 }
 
 export const IngredientsList: React.FC<ingredientsProps> = ({title}) => {
+    const data = [
+        {
+            name: "Baked Salmon",
+            ingredients: [
+                {name: "Salmon", amount:1, measurement: "l lb"},
+                {name: "Pine Nuts", amount:2, measurement: "cup"}
+            ],
+            steps: [
+                "aaaa",
+                "bbbb"
+            ]
+        },
+        {
+            name: "Fish Tacos",
+            ingredients: [
+                {name: "Whitefish", amount:1, measurement: "l lb"},
+                {name: "Tomatoes", amount:2, measurement: "large"}
+            ],
+            steps: [
+                "cccc",
+                "dddd"
+            ]
+        }
+    ]
     return (
         <h1>{title}</h1>
     )
