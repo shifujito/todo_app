a82d3b3f5cf8cc1811aad3927af42bbb957a7445
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2023-02-24 22:39:59 +0900

    [add] display　

--- front/src/components/Instructions.tsx
+++ front/src/components/Instructions.tsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+type InstructionsProps = {
+  title: string;
+  steps: string[];
+};
+
+const Instructions: React.FC<InstructionsProps> = ({ title, steps }) => {
+  return (
+    <section className="instructions">
+      <h2>{title}</h2>
+      {steps.map((s, i) => (
+        <p key={i}>{s}</p>
+      ))}
+    </section>
+  );
+};
