33bb87c7a1bdb3637b9804f1c3b58759b35e100e
shifujito <fujito.shion@gmail.com>
2023-06-08 00:57:01 +0900

    wip 日付入力を追加

--- front/src/components/inputTodo.tsx
+++ front/src/components/inputTodo.tsx
@@ -1,10 +1,19 @@
 import { Input, Button, Flex, Text, Center } from "@chakra-ui/react";
+import DatePicker from "react-datepicker";
 import { useState } from "react";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const InputTodo: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const handelInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value);
+  };
+
+  const Today = new Date();
+  const [startDate, setStartDate] = useState<Date | null>(Today);
+  const handleDateInput = (date: Date | null) => {
+    setStartDate(date);
   };
 
   return (
@@ -15,6 +24,9 @@ const InputTodo: React.FC = () => {
           value={todo}
           onChange={handelInput}
         />
+      </Center>
+      <Center h="100px" paddingLeft="10px">
+        <DatePicker selected={startDate} onChange={handleDateInput} />
       </Center>
       <Center h="100px" paddingLeft="10px">
         <Button colorScheme="blue">追加</Button>
