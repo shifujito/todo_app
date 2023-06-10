97f36ce20db0486606fe2a7560bf955ce9e44a95
shifujito <fujito.shion@gmail.com>
2023-06-10 16:46:55 +0900

    モック追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,13 @@
+import EditTodo from "./components/editTodo";
 import InputTodo from "./components/inputTodo";
 
 function App() {
-  return <InputTodo />;
+  return (
+    <>
+      <InputTodo />
+      <EditTodo />
+    </>
+  );
 }
 
 export default App;
---
+++ front/src/types.ts
@@ -0,0 +1,7 @@
+export type Todo = {
+    id: number;
+    title: string;
+    startDate: string;
+    endDate: string;
+    content: string
+}
